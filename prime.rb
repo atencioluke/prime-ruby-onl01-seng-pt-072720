36bff1fc34a8491d8376ab9137d45e7f098a9723
Luke Atencio <atencioluke@gmail.com>
2020-07-30 06:15:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,6 @@ def prime?(integer)
   if integer < 2
     return false
   else
-    num_check = [2..integer-1]
-    num_check.all? { |number| integer % number != 0 }
+    (2..integer-1).to_a.all? { |number| integer % number != 0 }
   end
 end
